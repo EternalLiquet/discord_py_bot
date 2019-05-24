ab91fc616bd01410516bd5c88e1eba4e92a3f717
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2019-05-23 22:28:29 -0400

    modify gitignore again cos i realized my fucking mistake

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+
+from discord.ext import commands
+
+DiscordToken = open("token.not_bananice") 
+
+DiscordClient = commands.Bot(command_prefix = 'n!')
+
+@DiscordClient.event
+async def on_ready():
+    print('Bot ready')
+
+DiscordClient.run(DiscordToken)
