ae30fbe4e9b95063347ed85fc221976bdae842f3
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2019-05-23 22:37:39 -0400

    add readline to open method

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 
 from discord.ext import commands
 
-DiscordToken = open("token.not_bananice") 
+DiscordToken = open("token.not_bananice").readline()
 
 DiscordClient = commands.Bot(command_prefix = 'n!')
 
